a187faaaa3aea03e3fac9e2b4431525ff3e7dbed
Thirawat sutalungka <thirawat@crmc.consulting>
2022-07-18 10:18:08 +0000

    update version 1 for transform data

---
+++ prism_convertor/src/main/java/se/chula/App.java
@@ -0,0 +1,37 @@
+package se.chula;
+
+import prism.Prism;
+import se.chula.prism.PrismFormatter;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import org.xml.sax.SAXException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String xmlPath = "/app/prism_convertor/xml/pta.xml";
+        
+        PrismFormatter formater = new PrismFormatter();
+        try {
+            formater.readXML(xmlPath);
+            String prism_code = formater.transform();
+            System.out.println(prism_code);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        
+        // Prism prism = new Prism(null);
+        
+    }
+
+}
---
+++ reference.md
@@ -0,0 +1,1 @@
+https://github.com/prismmodelchecker/prism/blob/master/prism/src/prism/PrismTest.java
